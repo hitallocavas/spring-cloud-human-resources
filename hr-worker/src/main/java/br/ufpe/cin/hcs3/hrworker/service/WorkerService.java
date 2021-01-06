c7ae7c385ef141b69f1bba4eeff4e2e27f04ee59
hitallocavas <hcs3@cin.ufpe.br>
2021-01-06 20:38:21 -0300

    Implementação de Balanceamento de Carga com Ribbon

--- hr-worker/src/main/java/br/ufpe/cin/hcs3/hrworker/service/WorkerService.java
+++ hr-worker/src/main/java/br/ufpe/cin/hcs3/hrworker/service/WorkerService.java
@@ -4,21 +4,27 @@ import br.ufpe.cin.hcs3.hrworker.domain.entity.Worker;
 import br.ufpe.cin.hcs3.hrworker.domain.repository.WorkerRepository;
 import br.ufpe.cin.hcs3.hrworker.transport.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WorkerService {
 
+    private final Environment environment;
     private final WorkerRepository repository;
 
-    public List<Worker> findAll(){
+    public List<Worker> findAll() {
         return this.repository.findAll();
     }
 
-    public Worker findById(Long id){
+    public Worker findById(Long id) {
+        String port = environment.getProperty("local.server.port");
+        log.info("Porta atual: " + port);
         return this.repository.findById(id).orElseThrow(NotFoundException::new);
     }
 
