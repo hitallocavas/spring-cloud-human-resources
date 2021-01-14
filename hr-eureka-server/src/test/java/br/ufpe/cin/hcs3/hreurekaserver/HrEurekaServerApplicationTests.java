ea7076e2d55be79b40c481599cd40bdc1ef2a988
hitallocavas <hcs3@cin.ufpe.br>
2021-01-14 12:48:39 -0300

    Criação do projeto HR Eureka Server

---
+++ hr-eureka-server/src/test/java/br/ufpe/cin/hcs3/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.hcs3.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
