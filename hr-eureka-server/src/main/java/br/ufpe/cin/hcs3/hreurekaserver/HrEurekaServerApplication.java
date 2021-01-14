bc06e2febed9854d9afa1fd3c800b07d001333e4
hitallocavas <hcs3@cin.ufpe.br>
2021-01-14 13:03:20 -0300

    Configuring Eureka Server

--- hr-eureka-server/src/main/java/br/ufpe/cin/hcs3/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/br/ufpe/cin/hcs3/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,9 @@ package br.ufpe.cin.hcs3.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
