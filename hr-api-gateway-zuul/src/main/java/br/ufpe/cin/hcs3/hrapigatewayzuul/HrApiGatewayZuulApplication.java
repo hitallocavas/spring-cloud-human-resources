2cd861b6d6df9cfc752a52874d3828532239cd97
hitallocavas <hcs3@cin.ufpe.br>
2021-03-07 09:50:23 -0300

    project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/main/java/br/ufpe/cin/hcs3/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package br.ufpe.cin.hcs3.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+    }
+
+}
---
+++ hr-api-gateway-zuul/src/test/java/br/ufpe/cin/hcs3/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.hcs3.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
